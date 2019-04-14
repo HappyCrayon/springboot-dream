fa7ff7791a02764101bbf3ead508347438c66aea
HappyCrayon <elitejiang2015@qq.com>
2019-04-15 03:19:50 +0800

    分布式事务预提交

--- fundsale-module-admin-server/src/test/java/com/springboot/admin/controller/MultiDBTest.java
+++ fundsale-module-admin-server/src/test/java/com/springboot/admin/controller/MultiDBTest.java
@@ -1,29 +1,46 @@
 package com.springboot.admin.controller;
 
-import com.springboot.admin.service.DepartmentService;
-import com.springboot.admin.service.EmployeeService;
+import com.springboot.admin.mapper.DepartmentMapper;
+import com.springboot.admin.mapper.ProductMapper;
+import com.springboot.common.datasource.DBTypeEnum;
+import com.springboot.common.datasource.DbContextHolder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MultiDBTest {
 
     @Autowired
-    private EmployeeService employeeService;
+    private DepartmentMapper departmentMapper;
 
     @Autowired
-    private DepartmentService departmentService;
+    private ProductMapper productMapper;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
 
     @Test
+    @Transactional
     public void testMultiDB() {
-        //DbContextHolder.setDbType(DBTypeEnum.db2);
+        //TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            DbContextHolder.setDbType(DBTypeEnum.db1);
+            departmentMapper.selectList(null).stream().forEach(item -> System.out.println(item));
 
-        employeeService.getEmployeeList().stream().forEach(item -> System.out.println(item));
+            DbContextHolder.setDbType(DBTypeEnum.db2);
+            productMapper.selectList(null).stream().forEach(item -> System.out.println(item));
 
-        departmentService.list(null).stream().forEach(item -> System.out.println(item));
+            //transactionManager.commit(status);
+        } catch (Exception e) {
+            //transactionManager.rollback(status);
+            throw e;
+        }
+
     }
 }
