307d59236fb780b523909cf083bef4af4876b83f
HappyCrayon <elitejiang2015@qq.com>
2019-05-05 22:24:42 +0800

    trade模块

---
+++ fundsale-module-trade-server/src/main/java/com/springboot/trade/configuration/MybatisPlusConfig.java
@@ -0,0 +1,69 @@
+package com.springboot.trade.configuration;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author DGD
+ * @date 2018/2/6.
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan(basePackages = {"com.springboot.admin.mapper"})
+public class MybatisPlusConfig {
+
+    /**
+     * mybatis-plus分页插件<br>
+     * 文档：http://mp.baomidou.com<br>
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        //paginationInterceptor.setLocalPage(true);// 开启 PageHelper 的支持
+        return paginationInterceptor;
+    }
+
+    @Bean(name = "datasource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid.datasource")
+    public DataSource datasource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+
+
+    @Bean("sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(datasource());
+        //指明mapper.xml位置(配置文件中指明的xml位置会失效用此方式代替，具体原因未知)
+        //sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:/mapper/**/*Mapper.xml"));
+
+        MybatisConfiguration configuration = new MybatisConfiguration();
+        configuration.setJdbcTypeForNull(JdbcType.NULL);
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCacheEnabled(false);
+        sqlSessionFactory.setConfiguration(configuration);
+        sqlSessionFactory.setPlugins(new Interceptor[]{ //PerformanceInterceptor(),OptimisticLockerInterceptor()
+                paginationInterceptor()
+        });
+        return sqlSessionFactory.getObject();
+    }
+
+//    @Bean
+//    public GlobalConfig globalConfig() {
+//        GlobalConfig conf = new GlobalConfig();
+//        return conf;
+//    }
+}
