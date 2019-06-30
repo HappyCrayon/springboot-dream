567e9f3c3182888e2b3e48dbed10512f242b7be9
HappyCrayon <elitejiang2015@qq.com>
2019-06-30 10:45:57 +0800

    删除多余类

--- fundsale-module-admin-server/src/main/java/com/springboot/admin/sqlparser/XMLScriptBuilderTest.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.admin.sqlparser;
-
-public class XMLScriptBuilderTest {
-    public static void main(String[] args) {
-        //XMLScriptBuilder xmlScriptBuilder = new XMLScriptBuilder(new Configuration(), new XNode());
-    }
-}
