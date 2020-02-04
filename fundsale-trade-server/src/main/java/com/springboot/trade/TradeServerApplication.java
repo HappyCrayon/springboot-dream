9043af9c3766eab834c1a054ff37e0c0795f9240
HappyCrayon <elitejiang2015@qq.com>
2020-02-04 12:24:09 +0800

    模块名称调整

--- fundsale-trade-server/src/main/java/com/springboot/trade/TradeServerApplication.java
+++ fundsale-trade-server/src/main/java/com/springboot/trade/TradeServerApplication.java
@@ -2,9 +2,10 @@ package com.springboot.trade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class TradeServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TradeServerApplication.class, args);
